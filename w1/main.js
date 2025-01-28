a75081ddf6ef856b00c86792c4893bc320f693b5
manveersingh00 <110972862+JATisha1@users.noreply.github.com>
2025-01-28 13:43:41 +0530

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log("Week 1 - Hello World");
+console.log("Hello from inside the main.js");
+
+//let myVar;
+const myVar = 42;
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log('myVarType ${myVarType}');
+
+if(myVarType ==="number") {
+    console.log(' will 11 this one run?')
+ 
+ } else {
+        console.log('Will 13 this one run');
+    }
+
+function runNow () {
+    console.log('inside the function');
+}
+
+
+runNow();
+runNow();
